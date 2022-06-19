308336f9afac1f194a0ca0e2dd4ef3f6f2e95704
Malene Ulm <msul36749@edu.ucl.dk>
2022-06-19 17:58:37 +0200

    inital commit

---
+++ recipe-app/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import store from './store'
+import './registerServiceWorker'
+
+
+
+const app = createApp(App).use(store)
+app.use(VueAxios, axios)
+app.use(router).mount('#app')
