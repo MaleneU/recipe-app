2ade7cc47a71e98b47e6eedd50e71066f7a10d1f
Malene Ulm <msul36749@edu.ucl.dk>
2022-06-22 22:22:12 +0200

    manifest color and icons

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,11 @@ module.exports = defineConfig({
 
 module.exports = {
   publicPath: '/recipe-app/',
+  pwa: {
+    name: "Cooking Time",
+    themeColor: "#FF5F5F",
+    icons: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+  }
 }
